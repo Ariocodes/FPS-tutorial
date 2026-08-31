82ee28ae4c0754d8be2650e9121f7eb3e0762b09
Ario <Ariobashiri@gmail.com>
2026-08-31 19:40:52 +0300

    Attack animations added, attacking mechanism added, bullets and guns added.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,22 +5,39 @@ public class Enemy : MonoBehaviour
 {
     private StateMachine stateMachine;
     private NavMeshAgent agent;
+    private GameObject player;
+    private bool wasMoving;
+
+    [Header("AI Navigation")]
     public NavMeshAgent Agent { get => agent; }
     public Path path;
+
+    public GameObject Player {  get => player; }
 
     // animations
     public Animator legAnimator;
     public Animator armAnimator;
-    private bool wasMoving;
 
-    // ONLY FOR DEBUGGING PURPOSES
-    [SerializeField]
-    private string currentState;
-    private GameObject player;
+
+
+
+    [Header("Sight Values")]
     public float sightDistance = 20f;
     public float fieldOfView = 85f;
     public float eyeHeight;
+    [Header("Weapon Values")]
+    public Transform gunBarrel;
+    [Range(0.1f, 10f)]
+    public float fireRate;
 
+
+
+
+
+    // ONLY FOR DEBUGGING PURPOSES
+    [SerializeField]
+    [Header("DEBUGGING")]
+    private string currentState;
 
     void Start()
     {
@@ -37,6 +54,11 @@ public class Enemy : MonoBehaviour
         CanSeePlayer();
         currentState = stateMachine.activeState.ToString();
     }
+
+
+
+
+
     // updating animation from idle to walking based on enemy movement.
     public void UpdateMovementAnimation()
     {
@@ -44,7 +66,15 @@ public class Enemy : MonoBehaviour
         if(isMoving != wasMoving)
         {
             legAnimator.Play(isMoving ? "legsAnimation" : "Idle");
-            armAnimator.Play(isMoving ? "armsAnimation" : "Idle");
+
+
+            bool isAttacking = stateMachine.activeState is AttackState;
+            bool isHolstering = armAnimator.GetCurrentAnimatorStateInfo(0).IsName("HolsterWeapon");
+
+            if (!isAttacking && !isHolstering)
+            {
+                armAnimator.Play(isMoving ? "armsAnimation" : "Idle");
+            }
             wasMoving = isMoving;
         }
     }
--- Assets/Scripts/Enemy/States/AttackState.cs
+++ Assets/Scripts/Enemy/States/AttackState.cs
@@ -5,24 +5,36 @@ public class AttackState : BaseState
 
     private float moveTimer;
     private float losePlayerTimer;
+
+
     [SerializeField]
-    private float waitBeforeSearchTime = 8; // time to stay in attack state when lost sight of player
-    public override void Enter()
-    {
+    private float waitBeforeSearchTime = 2; // time to stay in attack state when lost sight of player
+    private float shotTimer;
 
-    }
-
-    public override void Exit()
-    {
-
-    }
-
+    private bool weaponDrawn = false;
+    public override void Enter() { }
+    public override void Exit() { }
     public override void Perform()
     {
-        if (enemy.CanSeePlayer())
+        if (enemy.CanSeePlayer()) // if player can be seen
         {
+            if (!weaponDrawn)
+            {
+                enemy.armAnimator.Play("DrawWeapon");
+                weaponDrawn = true;
+            }
+            // lock the lose player timer and increment the move and shot timers.
             losePlayerTimer = 0;
             moveTimer += Time.deltaTime;
+            shotTimer += Time.deltaTime;
+            enemy.transform.LookAt(enemy.Player.transform);
+            
+            if(shotTimer > enemy.fireRate)
+            {
+                Shoot();
+            }
+
+
             if (moveTimer > Random.Range(3, 7))
             {
                 enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere * 5));
@@ -34,10 +46,32 @@ public class AttackState : BaseState
             losePlayerTimer += Time.deltaTime;
             if(losePlayerTimer > waitBeforeSearchTime)
             {
+                if (weaponDrawn)
+                {
+                    enemy.armAnimator.Play("HolsterWeapon");
+                    weaponDrawn = false;
+                }
                 // Change to search state.
                 stateMachine.ChangeState(new PatrolState());
             }
         }
+    }
+
+
+    public void Shoot()
+    {
+        // store reference to the gun barrel.
+        Transform gunbarrel = enemy.gunBarrel;
+
+
+        // instantiate a new bullet.
+        GameObject bullet = GameObject.Instantiate(Resources.Load("Prefabs/Bullet") as GameObject, gunbarrel.position, enemy.transform.rotation);
+        // calculate the direction to the playyer.
+        Vector3 shootDirection = (enemy.Player.transform.position - gunbarrel.transform.position).normalized;
+        // add force to rigidbody of the bullet.
+        bullet.GetComponent<Rigidbody>().linearVelocity = Quaternion.AngleAxis(Random.Range(-3f, 3f), Vector3.up) * shootDirection * 10000;
+        Debug.Log("SHOOT");
+        shotTimer = 0;
     }
 
 }
