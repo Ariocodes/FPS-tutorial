e74fd5cd85b2ac984ab670a25128f4807407dc28
Ario <Ariobashiri@gmail.com>
2026-09-07 23:55:02 +0300

    Enemy can now search multiple spots
    In SearchState the enemy can now look at multiple spots before going back to PatrolState

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -28,6 +28,9 @@ public class Enemy : MonoBehaviour
     public float sightDistance = 20f;
     public float fieldOfView = 85f;
     public float eyeHeight;
+    [Header("SearchState Values")]
+    [Range(1, 10)]
+    public int searchPoints = 3;
     [Range(0.1f, 20f)]
     public float losePlayerTime = 3f;
     [Header("Weapon Values")]
--- Assets/Scripts/Enemy/States/SearchState.cs
+++ Assets/Scripts/Enemy/States/SearchState.cs
@@ -3,10 +3,13 @@ using UnityEngine;
 public class SearchState : BaseState
 {
     private float searchTimer;
+    private float moveTimer;
+    private int searchPoints;
 
     public override void Enter()
     {
         enemy.Agent.SetDestination(enemy.LastKnownPlayerPosition);
+        searchPoints = enemy.searchPoints;
     }
 
     public override void Perform()
@@ -18,12 +21,18 @@ public class SearchState : BaseState
         if(enemy.Agent.remainingDistance < enemy.Agent.stoppingDistance * 10)
         {
             searchTimer += Time.deltaTime;
-            if(searchTimer > enemy.losePlayerTime)
+            moveTimer += Time.deltaTime;
+            if (moveTimer > Random.Range(2, 4) && searchPoints > 0)
+            {
+                enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere * 10));
+                moveTimer = 0;
+                searchPoints--;
+            }
+            if(searchTimer > enemy.losePlayerTime && searchPoints <= 0)
             {
                 stateMachine.ChangeState(new PatrolState());
             }
         }
-      
     }
 
     public override void Exit()
